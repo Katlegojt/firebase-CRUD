7182fc585a5f1df298b45320e7e70fa36b68bdd9
katlegojt <katlego209@gmail.com>
2020-12-11 15:15:57 +0200

    my commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,7 @@
 
 export const environment = {
   production: false,
-    firebaseConfig : {
-    apiKey: "AIzaSyABqGa_7USBixJLMxiZ87doKhHrijF7_-U",
-    authDomain: "my-cancer-guide-2020.firebaseapp.com",
-    databaseURL: "https://my-cancer-guide-2020.firebaseio.com",
-    projectId: "my-cancer-guide-2020",
-    storageBucket: "my-cancer-guide-2020.appspot.com",
-    messagingSenderId: "533019886340",
-    appId: "1:533019886340:web:67f5076f1ea81dd37d4651",
-    measurementId: "G-14ZFKKLZBG"
-  }
-};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
